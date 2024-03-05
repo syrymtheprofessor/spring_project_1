c21319a86063d81ef8e3edf5305ae6c7ce1c67c6
syrymtheprofessor <syrym.cappelo@gmail.com>
2024-03-05 12:53:40 +0500

    First commit to init

---
+++ src/test/java/com/example/students_in_springboot/StudentsInSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.students_in_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsInSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
