21780854273d1da6062772e3bd21b959f26b3c02
syrymtheprofessor <syrym.cappelo@gmail.com>
2024-03-14 00:01:19 +0500

    added getallstudents controller with lombok

--- src/main/java/com/example/students_in_springboot/controller/StudentController.java
+++ src/main/java/com/example/students_in_springboot/controller/StudentController.java
@@ -1,13 +1,22 @@
 package com.example.students_in_springboot.controller;
 
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.students_in_springboot.model.Student;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
-    public List<Student>
-
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return List.of(
+                Student.builder().firstName("Test").lastName("Test2").age(13).email("Test@mail.ru").build(),
+                Student.builder().firstName("Test").lastName("Test2").age(13).email("Test@mail.ru").build(),
+                Student.builder().firstName("Test").lastName("Test2").age(13).email("Test@mail.ru").build()
+        );
+    }
 }
--- src/main/java/com/example/students_in_springboot/model/Student.java
+++ src/main/java/com/example/students_in_springboot/model/Student.java
@@ -1,15 +1,25 @@
 package com.example.students_in_springboot.model;
 
+import lombok.*;
+
 import java.time.LocalDate;
 
-public class Student {
+/*
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor - если я поставлю @NotNull для email, то @NoArgsConstructor уже не будет работать
+потому что говорим по умолчанию нам нужен email
 
+но, это все можно заменить через @Data
+ */
+@Data
+@Builder
+public class Student {
     private String firstName;
     private String lastName;
     private String email;
     private LocalDate dateOfBirth;
     private int age;
-
-
-
 }
