aff53a6f3b084381e372841252af2a6be027a02a
syrymtheprofessor <syrym.cappelo@gmail.com>
2024-03-13 23:44:04 +0500

    lombok installed. added controller and model 'student'

---
+++ src/main/java/com/example/students_in_springboot/model/Student.java
@@ -0,0 +1,15 @@
+package com.example.students_in_springboot.model;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate dateOfBirth;
+    private int age;
+
+
+
+}
