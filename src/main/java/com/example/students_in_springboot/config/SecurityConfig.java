fd3c84ab55cc106471852276b7888c3dfec30287
syrymtheprofessor <syrym.cappelo@gmail.com>
2024-04-24 18:25:55 +0500

    Learn Security Filter Chain!CSRF http configurer. Http requests permitions

--- src/main/java/com/example/students_in_springboot/config/SecurityConfig.java
+++ src/main/java/com/example/students_in_springboot/config/SecurityConfig.java
@@ -2,13 +2,18 @@ package com.example.students_in_springboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.tags.form.AbstractHtmlElementBodyTag;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +23,15 @@ public class SecurityConfig {
         UserDetails admin = User.builder().username("syrym").password(encoder.encode("admin")).build();
 
         return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/users/").permitAll() //Сюда можно всем
+                        .requestMatchers("/api/v1/users/**").authenticated()) //Все пути после 'users/' отметил с '**'
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll) //Всем можно логиниться
+                .build();
     }
 
     @Bean
